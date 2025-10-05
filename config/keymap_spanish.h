a6e1708750b910053c924cb7ede72c1243dc3e47
ppurena <ppuren@gmail.com>
2025-10-05 09:23:55 -0600

    Adding spanish keyboard

--- config/keymap_spanish.h
+++ config/keymap_spanish.h
@@ -9,11 +9,11 @@
 
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │ \ │   │   │   │   │   │   │   │   │   │   │ ' │ ì │       │
+ * │   │   │   │   │   │   │   │   │   │   │   │ ' │ ¡ │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │ è │ + │     │
+ * │     │   │   │   │   │   │   │   │   │   │   │ ` │ + │     │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │ ò │ à │ ù │    │
+ * │      │   │   │   │   │   │   │   │   │   │ ñ │ ´ │   │    │
  * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
  * │    │ < │   │   │   │   │   │   │   │ , │ . │ - │          │
  * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
@@ -21,38 +21,36 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define ES_BSLH GRAVE            // backslash
 #define ES_SQUOT MINUS           // '
-#define ES_IGRV EQUAL            // ì
+#define ES_OPEXCL EQUAL          // ¡
 // Row 2
-#define ES_EGRV LBKT             // è
+#define ES_EGRV LBKT             // `
 #define ES_PLUS SLASH            // +
 // Row 3
-#define ES_OGRV SEMI             // ò
-#define ES_AGRV APOS             // à
-#define ES_UGRV NON_US_HASH      // ù
+#define ES_N    SEMI             // ñ
+#define ES_AGRV APOS             // ´
 // Row 4
 #define ES_LESS NON_US_BSLH      // <
 #define ES_MINUS RBKT            // -
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │ | │ ! │ " │ £ │ $ │ % │ & │ / │ ( │ ) │ = │ ? │ ^ │       │
+ * │   │ ! │ " │ · │ $ │ % │ & │ / │ ( │ ) │ = │ ? │ ¿ │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │ é │ * │     │
+ * │     │   │   │   │   │   │   │   │   │   │   │ ^ │ * │     │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │ ç │ ° │ § │    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
  * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
  * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
  * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
  * │    │    │    │                        │    │    │    │    │
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
-// Row 1
-#define ES_PIPE LS(GRAVE)        // |
+
+
 #define ES_EXCL LS(N1)           // !
 #define ES_DQUOT LS(N2)          // "
-#define ES_PND LS(N3)            // £
+#define ES_DOT LS(N3)            // ·
 #define ES_USD LS(N4)            // $
 #define ES_PERC LS(N5)           // %
 #define ES_AND LS(N6)            // &
@@ -61,14 +59,13 @@
 #define ES_RPAR LS(N9)           // )
 #define ES_EQL LS(N0)            // =
 #define ES_QUES LS(MINUS)        // ?
-#define ES_CARET LS(EQUAL)       // ^
+#define ES_OPQUES LS(EQUAL)      // ¿
 // Row 2
-#define ES_EACU LS(LBKT)         // é
+#define ES_EACU LS(LBKT)         // ^
 #define ES_AST LS(RBKT)          // *
 // Row 3
-#define ES_CCED LS(SEMI)         // ç
-#define ES_DEG LS(APOS)          // °
-#define ES_SECT LS(NON_US_HASH)  // §
+//#define ES_DEG LS(APOS)          // °
+//#define ES_SECT LS(NON_US_HASH)  // §
 // Row 4
 #define ES_GREAT LS(NON_US_BSLH) // >
 #define ES_UNDER LS(SLASH)       // _
@@ -86,37 +83,18 @@
  * │    │    │    │                        │    │    │    │    │
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
+// Row 1
+#define ES_BSLH RA(GRAVE)        
+#define ES_PIPE RA(N1)           // |
+#define ES_AT RA(N2)             // @
+#define ES_HASH RA(N3)           // #
+#define ES_TILDE RA(N3)          // ~
+
 // Row 2
-#define ES_EUR RA(E)             // ñ
+#define ES_EUR  RA(E)            // €
 #define ES_LBKT RA(LBKT)         // [
 #define ES_RBKT RA(RBKT)         // ]
 // Row 3
-#define ES_AT RA(SEMI)           // @
-#define ES_HASH RA(APOS)         // #
+#define ES_LBRC RA(SEMI)         // {
+#define ES_RBRC RA(APOS)         // }
 
-/* Shift+AltGr symbols
- * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │
- * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │ { │ } │     │
- * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
- * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │   │   │   │   │   │   │   │   │   │   │   │          │
- * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
- * │    │    │    │                        │    │    │    │    │
- * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
- */
-// Row 2
-#define ES_LBRC LS(RA(LBKT))     // {
-#define ES_RBRC LS(RA(RBKT))     // }
-
-/** 
- * Personalized symbols.
- * 
- * These symbols are not part of the default italian input.
- * I use a custom windows keymap to input them.
-*/
-#define ES_BKTK RA(MINUS)        // `
-#define ES_TILDE RA(EQUAL)       // ~
-#define ES_CEGR RA(LS(E))        // È
